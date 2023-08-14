841df8af70cc54813aa240dc948e969c9b94e1b8
Shreevani Rao <123049057+ShreevaniRao@users.noreply.github.com>
2023-08-14 00:12:46 -0500

    Update minmax.py

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -18,7 +18,18 @@ print(f"The minimum value is: {max(strings,key=len)}")
 
 # TODO: define a custom "key" function to extract a data field
 
+def getmag(dataitem):
+    magnitude = dataitem["properties"]["mag"]
+    if (magnitude is None):
+        magnitude = 0
+    return float(magnitude)
 
-# TODO: open the data file and load the JSON
-# with open("../../30DayQuakes.json", "r") as datafile:
-#     data = json.load(datafile)
+
+# open the data file and load the JSON
+with open("../../30DayQuakes.json", "r") as datafile:
+    data = json.load(datafile)
+
+print(data["metadata"]["title"])
+print(len(data["features"]))
+print(min(data["features"], key=getmag))
+print(max(data["features"], key=getmag))
