20969a3487a9b2b8afb3d4d811b7de3cdc08ea43
Shreevani Rao <123049057+ShreevaniRao@users.noreply.github.com>
2023-08-24 00:12:16 -0500

    Update basiclog_start.py

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,19 @@
 # demonstrate the logging api in Python
+# use the built-in logging module
+import logging
 
-# TODO: use the built-in logging module
+logging.basicConfig(level=logging.DEBUG,
+                    filemode="w",
+                    filename="output.log")
 
+# Try out each of the log levels
+logging.debug("This is a debug-level log message")
+logging.info("This is an info-level log message")
+logging.warning("This is a warning-level message")
+logging.error("This is an error-level message")
+logging.critical("This is a critical-level message")
 
-# TODO: Use basicConfig to configure logging
+# Output formatted string to the log
+logging.info("Here's a {} variable and an int: {}".format("string", 10))
 
-# TODO: Try out each of the log levels
-
-# TODO: Output formatted strings to the log
 
