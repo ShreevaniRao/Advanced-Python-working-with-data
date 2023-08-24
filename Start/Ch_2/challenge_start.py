8b6d54e6a28c2589187a772744c9857f557b2cb0
Shreevani Rao <123049057+ShreevaniRao@users.noreply.github.com>
2023-08-24 00:08:50 -0500

    Update challenge_start.py

--- Start/Ch_2/challenge_start.py
+++ Start/Ch_2/challenge_start.py
@@ -2,8 +2,16 @@
 # Programming challenge: use advanced data collections on the earthquake data
 
 import json
-
+from collections import defaultdict
 
 # open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("./30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
+
+eCounter = defaultdict(int)
+
+# TODO: Count the elements in the list
+for q in data["features"]:
+   eCounter[q['properties']['type']] += 1
+   
+print(eCounter)
