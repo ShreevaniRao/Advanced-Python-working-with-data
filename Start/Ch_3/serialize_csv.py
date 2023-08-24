f8aa8968e033acaeec5c2f8cf5ae05100838fc60
Shreevani Rao <123049057+ShreevaniRao@users.noreply.github.com>
2023-08-24 00:11:55 -0500

    Update serialize_csv.py

--- Start/Ch_3/serialize_csv.py
+++ Start/Ch_3/serialize_csv.py
@@ -1,9 +1,9 @@
 # Example file for Advanced Python: Working With Data by Joe Marini
-# demonstrates how to serialize data to a CSV file
+# demonstrates how to serialize data to a JSON file
 
-import csv
 import json
 import datetime
+
 
 # read in the contents of the JSON file
 with open("./30DayQuakes.json", "r") as datafile:
@@ -12,28 +12,26 @@ with open("./30DayQuakes.json", "r") as datafile:
 
 def isbig(x):
     mag = x["properties"]["mag"]
-    return mag is not None and mag > 5
+    return mag is not None and mag > 6
 
 
-# Filter the data by quakes that are larger than 5 magnitude
+# TODO: define a function to transform complex JSON to simpler JSON
+def simplequake(quake):
+    return{
+        "place" : quake["properties"]["place"],
+         "mag"  : quake["properties"]["mag"],
+          "url" : quake["properties"]["url"],
+          "date" : str(datetime.date.fromtimestamp(int(quake["properties"]["time"]/1000))) 
+    }
+    
+
+# filter the data to only include large quakes
 largequakes = list(filter(isbig, data["features"]))
-
-# TODO: Create the header and row structures for the data
-header = ["Place","Magnititude","Date","Link"]
-rows =[]
-# TODO: populate the rows with the resulting quake data
-for quake in largequakes:
-    thedate = datetime.date.fromtimestamp(int(quake["properties"]["time"]/1000))
-    rows.append(
-        [quake["properties"]["place"],
-                 quake["properties"]["mag"],
-                 thedate,
-                 quake["properties"]["url"]]
-            )
-                 
-# TODO: write the results to the CSV file
-
-with open("largequakes.csv","w") as csvfile:
-    writer = csv.writer(csvfile, delimiter=",")
-    writer.writerow(header)
-    writer.writerows(rows)
+# TODO: transform the data to a JSON format we want to save
+largequakes = list(map(simplequake, largequakes))
+# TODO: use the dumps() function to write json to a string
+# str = json.dumps(largequakes, sort_keys = True, indent=4)
+# print(str)
+# TODO: use the dump() function to write json to a file
+with open("largequakes.json", "w") as outfile:
+    json.dump(largequakes, outfile, sort_keys=True , indent=4)
