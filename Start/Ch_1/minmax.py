3d7c5bc181243507cc4def0659ef490ebfccd3fd
Shreevani Rao <123049057+ShreevaniRao@users.noreply.github.com>
2023-08-24 00:05:42 -0500

    Update minmax.py

--- Start/Ch_1/minmax.py
+++ Start/Ch_1/minmax.py
@@ -9,25 +9,23 @@ strings = ["one", "three", "five", "seven", "eleven", "eighteen"]
 
 
 # TODO: The min() function finds the minimum value
-print(f"The minimum value is: {min(values)}")
-print(f"The minimum value is: {min(strings,key=len)}")
-
+print(f"min value in values is {min(values)}")
+print(f"min value in strings is {min(strings, key=len)}")
 # TODO: The max() function finds the maximum value
-print(f"The minimum value is: {max(values)}")
-print(f"The minimum value is: {max(strings,key=len)}")
+
+print(f"min value in values is {max(values)}")
+print(f"min value in strings is {max(strings, key=len)}")
 
 # TODO: define a custom "key" function to extract a data field
-
 def getmag(dataitem):
     magnitude = dataitem["properties"]["mag"]
     if (magnitude is None):
         magnitude = 0
     return float(magnitude)
 
-
-# open the data file and load the JSON
-with open("../../30DayQuakes.json", "r") as datafile:
-    data = json.load(datafile)
+# TODO: open the data file and load the JSON
+with open("./30DayQuakes.json", "r") as datafile:
+     data = json.load(datafile)
 
 print(data["metadata"]["title"])
 print(len(data["features"]))
